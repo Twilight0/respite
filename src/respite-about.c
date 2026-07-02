3c8f735289ad32812f0744eab7742b5875e6748e
Twilight0 <twilight0@vivaldi.net>
2026-07-03 00:21:22 +0300

    Fix license dialog newlines (\\n -> \n), verify next button placement

--- src/respite-about.c
+++ src/respite-about.c
@@ -67,7 +67,7 @@ void respite_about(GtkWindow *parent) {
         "comments", _("A modern simple media player."),
         "documenters", documenters,
         "copyright", copyrights,
-        "license", "This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.\\n\\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.\\n\\nYou should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.",
+        "license", "This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.\n\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.",
         "logo-icon-name", "org.gnome.Respite",
         "program-name", _("Respite Media Player"),
         "translator-credits", _("translator-credits"),
